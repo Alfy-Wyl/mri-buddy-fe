32bb4772e2c6626550124d8b3c4bc6faa31b94e3
Alfred William Nunoo <alfred.wnunoo@gmail.com>
2023-03-29 23:16:55 +0100

    includes safety questionnaire and backend url

---
+++ src/constants/config.js
@@ -0,0 +1,1 @@
+export const BACKEND_URL = "https://still-dust-2666.fly.dev/";
---
+++ src/constants/question.js
@@ -0,0 +1,47 @@
+export const questions = [
+  {
+    title: "Do you have a CARDIAC PACEMAKER or CARDIAC DEFIBRILLATOR?",
+    choices: ["Yes", "NO"],
+  },
+  {
+    title:
+      "Do you have a STENT, FILTER, CLIP or COIL in any part of your blood vessel?",
+    choices: ["Yes", "NO"],
+  },
+  {
+    title:
+      "Do you have a METAL PLATE/SCREW, or JOINT REPLACEMENT in any part of your bones?",
+    choices: ["Yes", "NO"],
+  },
+  {
+    title:
+      "Do you have any MEDICAL DEVICE such as COCHLEAR IMPLANT, NEUROSTIMULATOR, DRUG PORT, HRT PATCH, attached or implanted underneath your skin?",
+    choices: ["Yes", "NO"],
+  },
+  {
+    title: "Have you had an injury to your eyes involving metal?",
+    choices: ["Yes", "NO"],
+  },
+  {
+    title:
+      "Have you ever been involved in any GUNSHOT or BOMB-BLASTING EXPERIENCE?",
+    choices: ["Yes", "NO"],
+  },
+  {
+    title: "Do you have any REMOVABLE METAL DENTURES?",
+    choices: ["Yes", "NO"],
+  },
+  {
+    title: "Do you wear HEARING AIDS?",
+    choices: ["Yes", "NO"],
+  },
+  {
+    title: "Do you have an ARTIFICIAL LIMB?",
+    choices: ["Yes", "NO"],
+  },
+  {
+    title: "Are you PREGNANT?",
+    choices: ["Yes", "NO", "N/A"],
+    isSubmit: true,
+  },
+];
